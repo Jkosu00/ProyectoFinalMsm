b0daf8692ce6338eb8e0346be4c2e96ab9836f51
Jkosu00 <ea24016@ues.edu.sv>
2024-11-26 22:47:41 -0600

    Update script.js
    cambios en la ruta para el correcto funcionamiento de los assets

--- Maps/script.js
+++ Maps/script.js
@@ -1,27 +1,27 @@
 // Datos del slider: imágenes principales, textos y galería de imágenes adicionales
 const sliderData = [
     {
-        buttonImage: 'assets/haven/Haven_button.webp',
-        mainImage: 'assets/haven/Haven_mini-map.webp',
+        buttonImage: 'assets/Haven/Haven_button.webp',
+        mainImage: 'assets/Haven/Haven_mini-map.webp',
         title: 'Haven',
         text: 'Haven es un mapa de Valorant caracterizado por su diseño único con tres sitios de bomba (A, B y C), lo que lo convierte en un desafío tanto para los atacantes como para los defensores. El mapa tiene una estructura central abierta con múltiples rutas que pueden ser utilizadas para flanquear, lo que requiere un juego táctico y bien coordinado. Las zonas clave incluyen el "Mid", que conecta las tres áreas de ataque y defiende, y "A Long" y "C Long", que son puntos de acceso largos y estrechos. Los defensores deben ser muy ágiles para cubrir todos los sitios a la vez, mientras que los atacantes deben planear ataques diversificados.',
         gallery: [
-            { src: 'assets/haven/omenB.png' },
-            { src: 'assets/haven/razeB.png' },
-            { src: 'assets/haven/skyeB.png' },
-            { src: 'assets/haven/viperB.png' }
+            { src: 'assets/Haven/omenB.png' },
+            { src: 'assets/Haven/razeB.png' },
+            { src: 'assets/Haven/skyeB.png' },
+            { src: 'assets/Haven/viperB.png' }
         ]
     },
     {
-        buttonImage: 'assets/pearl/Button_Pearl.webp',
-        mainImage: 'assets/pearl/Pearl_minimap.webp',
+        buttonImage: 'assets/Pearl/Button_Pearl.webp',
+        mainImage: 'assets/Pearl/Pearl_minimap.webp',
         title: 'Pearl',
         text: 'Pearl es un mapa en Valorant con un diseño lineal, centrado en un área urbana subacuática, que combina calles abiertas y pasillos estrechos. Tiene dos sitios de bomba (A y B), con un centro en el que el control del "Mid" es esencial para ambos equipos. Los atacantes suelen tener que lidiar con espacios abiertos, mientras que los defensores pueden aprovechar varias elevaciones y ángulos estrechos. El mapa tiene una buena cantidad de cobertura, pero también exige buena coordinación para mantener el control de las áreas clave, como el "Mid" y las entradas a los sitios de bomba.',
         gallery: [
-            { src: 'assets/pearl/razeB.png' },
-            { src: 'assets/pearl/cloveB.png' },
-            { src: 'assets/pearl/breachB.png' },
-            { src: 'assets/pearl/sovaB.webp' }
+            { src: 'assets/Pearl/razeB.png' },
+            { src: 'assets/Pearl/cloveB.png' },
+            { src: 'assets/Pearl/breachB.png' },
+            { src: 'assets/Pearl/sovaB.webp' }
         ]
     },
     {
@@ -37,51 +37,51 @@ const sliderData = [
         ]
     },
     {
-        buttonImage: 'assets/split/split_button.png',
-        mainImage: 'assets/split/split_minimap.png',
+        buttonImage: 'assets/Split/split_button.png',
+        mainImage: 'assets/Split/split_minimap.png',
         title: 'Split',
         text: 'Split es un mapa de Valorant centrado en el control del "Mid", que conecta las dos áreas de ataque y defensa con varias rutas verticales y estrechas. El mapa está dividido por un gran muro central, creando dos secciones que requieren una coordinación perfecta entre los atacantes para superarlo. Los sitios de bomba A y B están ubicados en extremos opuestos del mapa, y el "Mid" es esencial para la rotación y el control. Las zonas verticales, como los techos y los elevadores, añaden un desafío adicional a los jugadores, haciendo que los movimientos sean dinámicos y ágiles.',
         gallery: [
-            { src: 'assets/split/jettB.png' },
-            { src: 'assets/split/cloveB.png' },
-            { src: 'assets/split/razeB.png' },
-            { src: 'assets/split/breachB.png' }
+            { src: 'assets/Split/jettB.png' },
+            { src: 'assets/Split/cloveB.png' },
+            { src: 'assets/Split/razeB.png' },
+            { src: 'assets/Split/breachB.png' }
         ]
     },
     {
-        buttonImage: 'assets/icebox/Icebox_button.png',
-        mainImage: 'assets/icebox/icebox_mininap.png',
+        buttonImage: 'assets/Icebox/Icebox_button.png',
+        mainImage: 'assets/Icebox/icebox_mininap.png',
         title: 'Icebox',
         text: 'Icebox es un mapa de Valorant que se caracteriza por su diseño vertical y cerrado, con una gran cantidad de áreas elevadas y pasillos estrechos. Los sitios de bomba A y B están rodeados por plataformas y escaleras, lo que ofrece muchas oportunidades para moverse por las alturas y sorprender a los enemigos. El "Mid" es clave para la rotación, ya que conecta las dos secciones del mapa y permite a los atacantes crear presión desde diferentes ángulos. Las rutas abiertas y las plataformas elevadas también favorecen a los agentes con movilidad y habilidades de control del espacio.',
         gallery: [
-            { src: 'assets/icebox/chamberB.png' },
-            { src: 'assets/icebox/viperB.png' },
-            { src: 'assets/icebox/sovaB.webp' },
-            { src: 'assets/icebox/razeB.png' }
+            { src: 'assets/Icebox/chamberB.png' },
+            { src: 'assets/Icebox/viperB.png' },
+            { src: 'assets/Icebox/sovaB.webp' },
+            { src: 'assets/Icebox/razeB.png' }
         ]
     },
     {
-        buttonImage: 'assets/abyss/abyss_button.webp',
-        mainImage: 'assets/abyss/abyss_minimap.png',
+        buttonImage: 'assets/Abyss/abyss_button.webp',
+        mainImage: 'assets/Abyss/abyss_minimap.png',
         title: 'Abyss',
         text: 'Abyss es un nuevo mapa en Valorant que destaca por su jugabilidad de alto riesgo y alta recompensa. Está diseñado con áreas verticales y trampas mortales que obligan a los jugadores a tener cuidado, ya que caer del mapa puede significar la muerte. El mapa tiene dos sitios de bomba, una distribución de tres carriles y rutas en el "Mid" que se ramifican, lo que permite múltiples estrategias. Las plataformas estrechas y las áreas elevadas hacen que la movilidad y la posicionamiento sean clave, mientras que las caídas accidentalmente pueden cambiar el curso de una ronda',
         gallery: [
-            { src: 'assets/abyss/sovaB.webp' },
-            { src: 'assets/abyss/breachB.png' },
-            { src: 'assets/abyss/omenB.png' },
-            { src: 'assets/abyss/jettB.png' }
+            { src: 'assets/Abyss/sovaB.webp' },
+            { src: 'assets/Abyss/breachB.png' },
+            { src: 'assets/Abyss/omenB.png' },
+            { src: 'assets/Abyss/jettB.png' }
         ]
     },
     {
-        buttonImage: 'assets/sunset/Sunset_button.webp',
-        mainImage: 'assets/sunset/Sunset_minimap.webp',
+        buttonImage: 'assets/Sunset/Sunset_button.webp',
+        mainImage: 'assets/Sunset/Sunset_minimap.webp',
         title: 'Sunset',
         text: 'Sunset, con un ambiente al estilo de la "hora dorada". Presenta largos pasillos que se cruzan entre sí, lo que facilita un juego metódico y controlado, similar a los mapas de Counter-Strike. La estética del mapa está inspirada en un vecindario de Boyle Heights, en Los Ángeles, donde se encuentra una instalación secreta de la facción Kingdom, y que ahora está siendo invadido por extrañas criaturas que se escaparon de dicha instalación. Los jugadores pueden interactuar con una puerta mecanizada que conecta el mercado de B con el patio central de medio, lo que añade una capa estratégica adicional al juego',
         gallery: [
-            { src: 'assets/sunset/skyeB.png' },
-            { src: 'assets/sunset/neonB.png' },
-            { src: 'assets/sunset/cloveB.png' },
-            { src: 'assets/sunset/omenB.png' }
+            { src: 'assets/Sunset/skyeB.png' },
+            { src: 'assets/Sunset/neonB.png' },
+            { src: 'assets/Sunset/cloveB.png' },
+            { src: 'assets/Sunset/omenB.png' }
         ]
     },
     
